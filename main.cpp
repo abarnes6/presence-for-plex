a3dc81a8a30043cdfccf10728c72a5053598e91e
Andrew <abarnes6@proton.me>
2025-04-10 16:37:56 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// Plex Rich Presence.cpp : Defines the entry point for the application.
+//
+
+#include "Plex Rich Presence.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
